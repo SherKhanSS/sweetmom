9458615b25e8878e2123cce558f1881b859740ef
Юрий Гуров <gurov@sweetchild.local>
2019-09-10 17:58:24 +0300

    Базовая стилизация

--- source/js/nav.js
+++ source/js/nav.js
@@ -13,14 +13,28 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-$(".site-list__item").click(function() {
-  var index = $(this).parent().children().index(this);
-  $('.site-list__item').removeClass('site-list__item--active');
-  $('.site-list__item:nth-child(' + (index + 1) + ')').addClass('site-list__item--active');
+jQuery("document").ready(function($){
+
+	var nav = $('.page-header');
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			nav.addClass("page-header--scroll");
+		} else {
+			nav.removeClass("page-header--scroll");
+		}
+	});
+
 });
 
-$(".sub-nav__item").click(function() {
-  var index = $(this).parent().children().index(this);
-  $('.sub-nav__item').removeClass('sub-nav__item--active');
-  $('.sub-nav__item:nth-child(' + (index + 1) + ')').addClass('sub-nav__item--active');
-});
+// $(".site-list__item").click(function() {
+//   var index = $(this).parent().children().index(this);
+//   $('.site-list__item').removeClass('site-list__item--active');
+//   $('.site-list__item:nth-child(' + (index + 1) + ')').addClass('site-list__item--active');
+// });
+//
+// $(".sub-nav__item").click(function() {
+//   var index = $(this).parent().children().index(this);
+//   $('.sub-nav__item').removeClass('sub-nav__item--active');
+//   $('.sub-nav__item:nth-child(' + (index + 1) + ')').addClass('sub-nav__item--active');
+// });
